299ff74b32e518ac1293c552b1ac910dc48f96b9
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-11-17 10:49:22 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,4 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS hstore;
+
 CREATE TABLE usuarios (
   id SERIAL PRIMARY KEY,
   data JSONB
+);
+
+CREATE TABLE productos(
+  id SERIAL PRIMARY KEY,
+  precio NUMERIC,
+  atributos JSONB,
+  atributosH HSTORE
 );
