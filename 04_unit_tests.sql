f5c6778534ea0611c14abd5903f8b030747ef7a8
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-11-17 10:54:37 -0600

    Update 04_unit_tests.sql

--- 04_unit_tests.sql
+++ 04_unit_tests.sql
@@ -25,3 +25,33 @@ BEGIN
   END IF;
 END;
 $$;
+
+--EJERCICIO
+
+DO $$
+BEGIN
+  IF EXISTS (
+    SELECT 1 FROM productos WHERE id = 1 AND atributos->>'COLOR' = 'ROJO'
+  ) THEN
+    RAISE NOTICE 'OK: color correcto para id 1';
+  ELSE
+    RAISE EXCEPTION 'Fallo: color incorrecto para id 1';
+  END IF;
+
+  IF EXISTS (
+    SELECT 1 FROM productos WHERE id = 1 AND atributos->>'CATEGORIA' = 'PAPELERIA'
+  ) THEN
+    RAISE NOTICE 'OK: categoria correcta para id 1';
+  ELSE
+    RAISE EXCEPTION 'Fallo: categoria incorrecta para id 1';
+  END IF;
+
+  IF EXISTS (
+    SELECT 1 FROM productos WHERE id = 2 AND atributos->>'TAMANO' = 'PEQUENO'
+  ) THEN
+    RAISE NOTICE 'OK: tamaño correcto para id 2';
+  ELSE
+    RAISE EXCEPTION 'Fallo: tamaño incorrecto para id 2';
+  END IF;
+END;
+$$;
